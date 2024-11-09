5a5abc9c3bb8d0c53e6430c8c691fd1ef8781869
zackcglee <leechanggong@gmail.com>
2024-11-09 11:51:00 +0900

    init

---
+++ platforms/windows/huginsetup/CopyInstallFiles.bat
@@ -0,0 +1,76 @@
+copy /Y ..\..\..\build\src\hugin1\hugin\Release\*.* files\bin
+
+copy /Y ..\..\..\build\src\hugin1\icpfind\Release\icpfind.exe files\bin
+copy /Y ..\..\..\build\src\hugin1\icpfind\Release\icpfindlib.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\icpfind\Release\jpeg62.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\icpfind\Release\libpng16.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\icpfind\Release\lzma.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\icpfind\Release\tiff.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\icpfind\Release\wxbase315u_vc_custom.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\icpfind\Release\wxmsw315u_core_vc_custom.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\icpfind\Release\zlib1.dll files\bin
+
+copy /Y ..\..\..\build\src\hugin_cpfind\cpfind\Release\lz4.dll files\bin
+
+copy /Y ..\..\..\build\src\hugin1\ptbatcher\Release\*.* files\bin
+copy /Y ..\..\..\build\src\hugin1\stitch_project\Release\*.* files\bin
+copy /Y ..\..\..\build\src\hugin1\executor\Release\*.* files\bin
+copy /Y ..\..\..\build\src\hugin1\calibrate_lens\Release\*.* files\bin
+
+copy /Y ..\..\..\build\src\hugin1\base_wx\Release\huginbasewx.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\base_wx\Release\jpeg62.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\base_wx\Release\libexpat.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\base_wx\Release\libpng16.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\base_wx\Release\lzma.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\base_wx\Release\tiff.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\base_wx\Release\wxbase315u_vc_custom.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\base_wx\Release\wxbase315u_xml_vc_custom.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\base_wx\Release\wxmsw315u_core_vc_custom.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\base_wx\Release\wxmsw315u_html_vc_custom.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\base_wx\Release\wxmsw315u_qa_vc_custom.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\base_wx\Release\wxmsw315u_xrc_vc_custom.dll files\bin
+copy /Y ..\..\..\build\src\hugin1\base_wx\Release\zlib1.dll files\bin
+
+copy /Y ..\..\..\build\src\celeste\Release\celeste.dll files\bin
+copy /Y ..\..\..\build\src\celeste\Release\celeste_standalone.exe files\bin
+copy /Y ..\..\..\build\src\deghosting\Release\deghosting_mask.exe files\bin
+copy /Y ..\..\..\build\src\hugin_cpfind\cpfind\Release\cpfind.exe files\bin
+copy /Y ..\..\..\build\src\hugin_cpfind\localfeatures\Release\localfeatures.dll files\bin
+copy /Y ..\..\..\build\src\hugin_script_interface\Release\hugin_python_interface.dll files\bin
+
+
+copy /Y ..\..\..\build\src\hugin_base\Release\huginbase.dll files\bin
+copy /Y ..\..\..\build\src\hugin_base\Release\glew32.dll files\bin
+copy /Y ..\..\..\build\src\hugin_base\Release\sqlite3.dll files\bin
+copy /Y ..\..\..\build\src\hugin_base\Release\exiv2.dll files\bin
+copy /Y ..\..\..\build\src\hugin_base\Release\iconv-2.dll files\bin
+copy /Y ..\..\..\build\src\hugin_base\Release\tiff.dll files\bin
+copy /Y ..\..\..\build\src\hugin_base\Release\zlib1.dll files\bin
+copy /Y ..\..\..\build\src\hugin_base\Release\lzma.dll files\bin
+copy /Y ..\..\..\build\src\hugin_base\Release\jpeg62.dll files\bin
+
+copy /Y ..\..\..\build\src\tools\Release\*.* files\bin
+
+copy /Y ..\..\..\vcpkg\buildtrees\openexr\x64-windows-rel\bin\IexMath-2_5.dll files\bin
+copy /Y ..\..\..\vcpkg\buildtrees\openexr\x64-windows-rel\bin\IlmImf-2_5.dll files\bin
+copy /Y ..\..\..\vcpkg\buildtrees\openexr\x64-windows-rel\bin\IlmImfUtil-2_5.dll files\bin
+copy /Y ..\..\..\vcpkg\buildtrees\openexr\x64-windows-rel\bin\IlmThread-2_5.dll files\bin
+copy /Y ..\..\..\vcpkg\buildtrees\gsl\x64-windows-rel\gsl.dll files\bin
+copy /Y ..\..\..\vcpkg\buildtrees\gsl\x64-windows-rel\gslcblas.dll files\bin
+
+copy /Y ..\..\..\libpano13-2021-05-17\pano13\build\tools\Release\panoinfo.exe  files\bin
+copy /Y ..\..\..\libpano13-2021-05-17\pano13\build\tools\Release\PTBatcherGUI.exe  files\bin
+copy /Y ..\..\..\libpano13-2021-05-17\pano13\build\tools\Release\PTblender.exe  files\bin
+copy /Y ..\..\..\libpano13-2021-05-17\pano13\build\tools\Release\PTcrop.exe  files\bin
+copy /Y ..\..\..\libpano13-2021-05-17\pano13\build\tools\Release\PTinfo.exe  files\bin
+copy /Y ..\..\..\libpano13-2021-05-17\pano13\build\tools\Release\PTmasker.exe  files\bin
+copy /Y ..\..\..\libpano13-2021-05-17\pano13\build\tools\Release\PTmasker.exe  files\bin
+copy /Y ..\..\..\libpano13-2021-05-17\pano13\build\tools\Release\PTmender.exe  files\bin
+copy /Y ..\..\..\libpano13-2021-05-17\pano13\build\tools\Release\PToptimizer.exe  files\bin
+copy /Y ..\..\..\libpano13-2021-05-17\pano13\build\tools\Release\PTroller.exe  files\bin
+copy /Y ..\..\..\libpano13-2021-05-17\pano13\build\tools\Release\PTtiff2psd.exe  files\bin
+copy /Y ..\..\..\libpano13-2021-05-17\pano13\build\tools\Release\PTtiffdump.exe  files\bin
+copy /Y ..\..\..\libpano13-2021-05-17\pano13\build\tools\Release\PTuncrop.exe  files\bin
+
+
+copy /Y _external_binaries\*.*  files\bin
